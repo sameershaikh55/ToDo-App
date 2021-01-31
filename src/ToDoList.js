567fbace3baeb500ffd6da93b4d61de36b69046c
sameer-shaikh <sameermubeen4@gmail.com>
2021-01-31 21:28:29 +0500

    Added New Updated

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,63 +1,69 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 import { List } from "./List";
 
 // importing material ui add-button
-import AddIcon from '@material-ui/icons/Add'; 
-
+import AddIcon from "@material-ui/icons/Add";
 
 function ToDoList() {
+	let [item, setItem] = useState("");
+	let [listItem, setListItem] = useState([]);
 
-  let [item, setItem] = useState("");
-  let [listItem, setListItem] = useState([]);
+	const changeInput = (event) => {
+		setItem(event.target.value);
+	};
 
-  const changeInput = (event) => {
-    setItem(event.target.value)
-  }
+	const updateList = (event) => {
+		event.preventDefault();
+		setListItem((listval) => {
+			return [...listval, item];
+		});
+		setItem("");
+	};
 
-  const updateList = (event) => {
-    event.preventDefault();
-    setListItem((listval) => {
-      return [...listval, item]
-    });
-    setItem("");
+	const deleteItem = (id) => {
+		setListItem((arritem) => {
+			return arritem.filter((arrayElement, index) => {
+				return index !== id;
+			});
+		});
+	};
 
-  }
-
-  const deleteItem = (id) => {
-    setListItem((arritem) => {
-      return arritem.filter((arrayElement, index) => {
-        return index !== id;
-      })
-    })
-  }
-
-  return (
-    <div className="fullPage">
-      <div className="box">
-        <form onSubmit={updateList}>
-          <h1>ToDo List</h1>
-          <div className="inputfield">
-            <input
-              required 
-              type="text" 
-              placeholder="Add an Item" 
-              value={item}
-              onChange={changeInput}  
-            />
-            <button className="addbutton">
-              <AddIcon className="icon"/>
-            </button>
-          </div>
-          <br />
-          <br />
-          {listItem.map((listExtend, index) => {
-            return <List  list={listExtend} key={index} id={index} onSelect={deleteItem} />;
-          })}
-        </form>
-      </div>
-    </div>
-  )
+	return (
+		<div className="fullPage">
+			<div className="box">
+				<form onSubmit={updateList}>
+					<h1>ToDo List</h1>
+					<div className="inputfield">
+						<input
+							required
+							type="text"
+							placeholder="Add an Item"
+							value={item}
+							onChange={changeInput}
+						/>
+						<button className="addbutton">
+							<AddIcon className="icon" />
+						</button>
+					</div>
+					<br />
+					<br />
+					<div className="ListContainer">
+						{listItem.map((listExtend, index) => {
+							return (
+								<List
+									list={listExtend}
+									key={index}
+									id={index}
+									onSelect={deleteItem}
+								/>
+							);
+						})}
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default ToDoList;
